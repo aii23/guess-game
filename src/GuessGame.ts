70d8e3e717504c5250d69c0ad960125d355fe69f
Aleksandr Ivlev <a.ivlev.om@gmail.com>
2024-08-09 13:51:14 +0300

    Added commit-reveal

--- src/GuessGame.ts
+++ src/GuessGame.ts
@@ -3,15 +3,28 @@ import {
   MerkleMap,
   MerkleMapWitness,
   Poseidon,
+  Provable,
   SmartContract,
   State,
+  Struct,
   method,
   state,
 } from 'o1js';
 
+export class HiddenValue extends Struct({
+  value: Field,
+  salt: Field,
+}) {
+  hash(): Field {
+    return Poseidon.hash([this.value, this.salt]);
+  }
+}
+
 export class GuessGame extends SmartContract {
   @state(Field) hiddenNumber = State<Field>();
   @state(Field) scoreRoot = State<Field>();
+  @state(Field) guessedNumber = State<Field>();
+  @state(Field) guesser = State<Field>();
 
   init() {
     super.init();
@@ -19,42 +32,68 @@ export class GuessGame extends SmartContract {
     this.scoreRoot.set(new MerkleMap().getRoot());
   }
 
-  @method async hideNumber(number: Field) {
+  @method async hideNumber(hiddenValue: HiddenValue) {
     let curHiddenNumber = this.hiddenNumber.getAndRequireEquals();
 
     curHiddenNumber.assertEquals(Field(0), 'Number is already hidden');
 
-    number.assertLessThan(Field(100), 'Value should be less then 100');
+    hiddenValue.value.assertLessThan(
+      Field(100),
+      'Value should be less then 100'
+    );
 
-    this.hiddenNumber.set(Poseidon.hash([number]));
+    this.hiddenNumber.set(hiddenValue.hash());
   }
 
-  @method async guessNumber(
-    number: Field,
+  @method async guessNumber(number: Field) {
+    let curGuessedNumber = this.guessedNumber.getAndRequireEquals();
+
+    curGuessedNumber.assertEquals(Field(0), 'You have already guessed number');
+
+    const sender = this.sender.getAndRequireSignature();
+    const senderHash = Poseidon.hash(sender.toFields());
+
+    this.guessedNumber.set(number);
+    this.guesser.set(senderHash);
+  }
+
+  @method async revealNumber(
+    hiddenValue: HiddenValue,
     score: Field,
     scoreWitness: MerkleMapWitness
   ) {
-    let curHiddenNumber = this.hiddenNumber.getAndRequireEquals();
-
-    curHiddenNumber.assertEquals(
-      Poseidon.hash([number]),
-      'Other numbre was guessed'
+    // Check hidden value
+    let currentHiddenNumber = this.hiddenNumber.getAndRequireEquals();
+    currentHiddenNumber.assertEquals(
+      hiddenValue.hash(),
+      'It is not hidden number'
     );
 
-    // Check witnessed value
+    // Check score witness
     const [prevScoreRoot, key] = scoreWitness.computeRootAndKeyV2(score);
 
     this.scoreRoot
       .getAndRequireEquals()
       .assertEquals(prevScoreRoot, 'Wrong score witness');
 
-    const sender = this.sender.getAndRequireSignature();
-    const senderHash = Poseidon.hash(sender.toFields());
-    key.assertEquals(senderHash, 'Witness for wrong user');
+    const guesserHash = this.guesser.getAndRequireEquals();
+    key.assertEquals(guesserHash, 'Witness for wrong user');
 
-    const [newScoreRoot] = scoreWitness.computeRootAndKeyV2(score.add(1));
+    // Check guess
+    const guessedNumber = this.guessedNumber.getAndRequireEquals();
+    const scoreDiff = Provable.if(
+      hiddenValue.value.equals(guessedNumber),
+      Field(1),
+      Field(0)
+    );
+
+    const [newScoreRoot] = scoreWitness.computeRootAndKeyV2(
+      score.add(scoreDiff)
+    );
 
     this.scoreRoot.set(newScoreRoot);
     this.hiddenNumber.set(Field(0));
+    this.guessedNumber.set(Field(0));
+    this.guesser.set(Field(0));
   }
 }
